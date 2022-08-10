baeba79f25a42ecc1a6faad9af5f5d3b54801667
Eduardo Almeida <eduardofralmeida745@gmail.com>
2022-08-09 21:34:03 -0300

    modulo 1

---
+++ app/app.ts
@@ -0,0 +1,10 @@
+import { NegociacaoController } from './controllers/negociacao-controller.js';
+import { NegociacoesView } from './views/negociacoes-view.js';
+
+const controller = new NegociacaoController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
+
